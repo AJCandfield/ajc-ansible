acd907cb6e1cb60f17bac8640f421fe7e40084f7
Alexander James Candfield <alex.candfield@gmail.com>
2022-02-19 22:55:14 +0100

    added installation script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+PATH_VENV="/tmp/venv"
+
+# sudo apt-get update
+# sudo apt-get --yes --force-yes install git python3
+sudo python -m venv ${PATH_VENV}
+
+source "${PATH_VENV}/bin/activate"
+
+sudo "${PATH_VENV}/bin/pip" install ansible
+
+"${PATH_VENV}/bin/ansible-playbook" playbooks/ping.yml
+"${PATH_VENV}/bin/ansible-playbook" playbooks/setup-workstation.yml
+
+# sudo rm -rf $PATH_VENV
+
+echo "Playbook run finished 👍‍"
