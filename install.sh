47e137d48560d3e03db7a7b69055615219457868
Alexander James Candfield <alex.candfield@gmail.com>
2022-02-20 01:14:03 +0100

    added verbosity to ansible

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ source "${PATH_VENV}/bin/activate"
 sudo "${PATH_VENV}/bin/pip" install ansible
 
 sudo "${PATH_VENV}/bin/ansible-playbook" playbooks/ping.yml
-sudo "${PATH_VENV}/bin/ansible-playbook" playbooks/setup-workstation.yml -vv
+sudo "${PATH_VENV}/bin/ansible-playbook" playbooks/setup-workstation.yml -vvv
 
 sudo rm -rf $PATH_VENV
 
