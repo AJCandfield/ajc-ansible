198aeca0d7bca08a9c15d008fca1ce4eb9f3edfa
Alexander James Candfield <alex.candfield@gmail.com>
2022-03-03 16:33:36 +0100

    small edit to main script

--- full-setup.sh
+++ full-setup.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -o errexit  # Exit on error
+set -o pipefail # Error if a command in pipeline returns non-zero code
+set -o nounset  # Error when an unset variable is evaluated
+
+source ./scripts/log.sh # https://github.com/Zordrak/bashlog
 
 # TODO: Implement check that git and python are installed
 
 PATH_VENV="/tmp/venv"
-TARGET_USER="alex"
+log debug "Python venv will be created at: ${PATH_VENV}"
 
-sudo apt-get --yes install python3-pip
-sudo apt-get --yes install python3-venv
-sudo apt-get --yes install python3-wheel
+TARGET_USER=$1
+log debug "Target User: ${TARGET_USER}"
+
+sudo apt-get --yes install \
+    python3-pip \
+    python3-venv \
+    python3-wheel
 
 sudo python3 -m venv ${PATH_VENV}
+log debug "Venv created."
 
 source "${PATH_VENV}/bin/activate"
 
@@ -19,6 +28,7 @@ sudo "${PATH_VENV}/bin/pip" install ansible
 
 "${PATH_VENV}/bin/ansible-playbook" playbooks/full_setup.yml -e "target_user=${TARGET_USER}" -vvv
 
-sudo rm -rf $PATH_VENV
+sudo rm -rf ${PATH_VENV}
+log debug "Venv removed."
 
-echo "Playbook run finished 👍‍"
+log info "Playbook run finished 👍‍"
